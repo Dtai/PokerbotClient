748d93a8d2ce403be6424600c780ad084a8175cb
Jonas <jonasvanthornhout@gmail.com>
2011-09-21 11:08:45 +0200

    Added an easier way to use the navigation bar in the documentation for the web. Added extra documentation for the program.

--- help/nav.js
+++ help/nav.js
@@ -1,3 +1,16 @@
 function href(url){
 	location.href = url;
 }
+
+function loadNavigation(id){	
+	var html = " <ul id=\"navigation\"> ";
+	html += "<li onclick=\"href('pokerWeb.html')\"> Poker</li>";
+	html += "<li onclick=\"href('ruleCreatorWeb.html')\"> RuleCreator</li>";
+	html += "<li onclick=\"href('tutorialWeb.html')\"> Tutorial</li>";
+	html += "<li onclick=\"href('featuresWeb.html')\"> Features</li>";
+	html += "<li onclick=\"href('functionsWeb.html')\"> Functions</li>";
+	html += "<li onclick=\"href('welcomeWindowWeb.html')\"> Welkomsvenster</li>";
+	html += "</ul> ";
+	
+	document.getElementById(id).innerHTML = html;
+}
