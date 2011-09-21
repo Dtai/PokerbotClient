6cde33bf23d12030fdf57386a7b624af458988f0
Jonas <jonasvanthornhout@gmail.com>
2011-09-21 11:46:17 +0200

    Added a highlighter for the navigation bar. Added more documentation about the game poker.

--- help/nav.js
+++ help/nav.js
@@ -4,13 +4,18 @@ function href(url){
 
 function loadNavigation(id){	
 	var html = " <ul id=\"navigation\"> ";
-	html += "<li onclick=\"href('pokerWeb.html')\"> Poker</li>";
-	html += "<li onclick=\"href('ruleCreatorWeb.html')\"> RuleCreator</li>";
-	html += "<li onclick=\"href('tutorialWeb.html')\"> Tutorial</li>";
-	html += "<li onclick=\"href('featuresWeb.html')\"> Features</li>";
-	html += "<li onclick=\"href('functionsWeb.html')\"> Functions</li>";
-	html += "<li onclick=\"href('welcomeWindowWeb.html')\"> Welkomsvenster</li>";
+	html += "<li id=\"poker\" onclick=\"href('pokerWeb.html')\"> Poker</li>";
+	html += "<li id=\"ruleCreator\" onclick=\"href('ruleCreatorWeb.html')\"> RuleCreator</li>";
+	html += "<li id=\"tutorial\" onclick=\"href('tutorialWeb.html')\"> Tutorial</li>";
+	html += "<li id=\"features\" onclick=\"href('featuresWeb.html')\"> Features</li>";
+	html += "<li id=\"functions\" onclick=\"href('functionsWeb.html')\"> Functions</li>";
+	html += "<li id=\"welcomeWindow\" onclick=\"href('welcomeWindowWeb.html')\"> Welkomsvenster</li>";
 	html += "</ul> ";
 	
 	document.getElementById(id).innerHTML = html;
+}
+
+function highlight(id){
+	document.getElementById(id).style.background = "#6e0600";
+	document.getElementById(id).style.color = "white";
 }
