db1c2c9784771b7c9aaa59835844bcb8a7366de6
Tias Guns <tias.guns@cs.kuleuven.be>
2012-11-09 11:46:40 +0100

    Compile.sh: comment out translation juggling, done automatically nowadays?

--- compile.sh
+++ compile.sh
@@ -3,12 +3,13 @@ cd build
 cmake ..
 make
 
-cd src/lib/pokerRuleSystem/CMakeFiles
-lupdate-qt4 pokerRuleSystem_nl_BE_lupdate.pro -ts pokerRuleSystem_nl_BE.ts
-mv pokerRuleSystem_nl_BE.ts ../../../../../translation/pokerRuleSystem_nl_BE.ts
+# Commented out: TS files are updated automatically?
+#cd src/lib/pokerRuleSystem/CMakeFiles
+#lupdate-qt4 pokerRuleSystem_nl_BE_lupdate.pro -ts pokerRuleSystem_nl_BE.ts
+#mv pokerRuleSystem_nl_BE.ts ../../../../../translation/pokerRuleSystem_nl_BE.ts
 
-cd ../../../../../build
+#cd ../../../../../build
 
-cd src/lib/ruleSystem/CMakeFiles
-lupdate-qt4 ruleSystem_nl_BE_lupdate.pro -ts ruleSystem_nl_BE.ts
-mv ruleSystem_nl_BE.ts ../../../../../translation/ruleSystem_nl_BE.ts
+#cd src/lib/ruleSystem/CMakeFiles
+#lupdate-qt4 ruleSystem_nl_BE_lupdate.pro -ts ruleSystem_nl_BE.ts
+#mv ruleSystem_nl_BE.ts ../../../../../translation/ruleSystem_nl_BE.ts
