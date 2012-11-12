0b6243868a479136129c5d224c5afa18ed1dd132
Tias Guns <tias.guns@cs.kuleuven.be>
2012-11-12 12:01:22 +0100

    Documenting getting the dll pack for the installer

--- installer/createInstaller.bat
+++ installer/createInstaller.bat
@@ -1,3 +1,7 @@
+:: first get the DLL pack from:
+:: https://github.com/Dtai/PokerbotClient/downloads 
+:: (that is also the place to upload the new installer)
+
 del setup.msi
 candle rc.wxs
 light rc.wixobj -loc WixUI_nl-nl.wxl -ext WixUIExtension.dll -ext WixUtilExtension.dll
